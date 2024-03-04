1aeb31bc5b4f45b5384bb8e11b8e8ad566979dfe
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2024-03-03 16:19:30 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+import Accordian from "./components/accordian";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordian Component */}
+      <Accordian />
+    </div>
+  );
+}
+
+export default App;
