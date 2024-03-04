77de37244bacc91f307d73f9925928e5df1d2134
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2024-03-04 12:17:49 -0800

    byilt star-rating component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { useState } from "react";
 import "./App.css";
 import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
       {/* Accordian Component */}
       {/* <Accordian /> */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      <StarRating noOfStars={10} />
     </div>
   );
 }
