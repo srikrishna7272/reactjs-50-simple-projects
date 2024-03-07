90d7942ce7261251a7f1e025c616df58f20f64f5
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2024-03-06 20:09:49 -0800

    built tab component without third party library.

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,22 @@ import menus from "./components/tree-view/data";
 import QRCodeGenerator from "./components/qr-code-generator";
 import LightDarkMode from "./components/light-dark-mode";
 import ScrollIndicator from "./components/scroll-indicator";
+import TabTest from "./components/custom-tabs/tab-test";
 
 function App() {
   return (
     <div className="App">
       {/* Accordian Component */}
-      {/* <Accordian /> */}
-      {/* <RandomColor /> */}
+      {/* <Accordian />
+      <RandomColor /> */}
       {/* <StarRating noOfStars={10} /> */}
       {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"} /> */}
       {/* <LoadMoreData /> */}
       {/* <TreeView menus={menus} /> */}
       {/* <QRCodeGenerator /> */}
       {/* <LightDarkMode /> */}
-      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
+      {/* <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} /> */}
+      <TabTest />
     </div>
   );
 }
